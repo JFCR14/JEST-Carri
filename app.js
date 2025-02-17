164911ebc5fa920b2b19c7659e0f06b78ea78ec7
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-02-17 18:42:41 +0000

    2.0

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
-// app.js
-
-// Función para sumar dos números
 const sum = (a, b) => {
     return a + b;
 };
 
-console.log(sum(7, 3)); // Para probar en consola
+console.log(sum(7, 3)); 
 
-// Tasas de conversión
+
 let oneEuroIs = {
-    "JPY": 156.5, // 1 EUR = 156.5 JPY
-    "USD": 1.07,  // 1 EUR = 1.07 USD
-    "GBP": 0.87   // 1 EUR = 0.87 GBP
+    "JPY": 156.5, 
+    "USD": 1.07,  
+    "GBP": 0.87   
 };
 
-// Función para convertir de euros a dólares
+
 function fromEuroToDollar(euros) {
     return euros * oneEuroIs.USD;
 }
 
-// Función para convertir de dólares a yenes
+
 function fromDollarToYen(dollars) {
-    let euros = dollars / oneEuroIs.USD; // Convertimos USD a EUR
-    return euros * oneEuroIs.JPY; // Convertimos EUR a JPY
+    let euros = dollars / oneEuroIs.USD; 
+    return euros * oneEuroIs.JPY; 
 }
 
-// Función para convertir de yenes a libras esterlinas
+
 function fromYenToPound(yen) {
-    let euros = yen / oneEuroIs.JPY; // Convertimos JPY a EUR
-    return euros * oneEuroIs.GBP; // Convertimos EUR a GBP
+    let euros = yen / oneEuroIs.JPY; 
+    return euros * oneEuroIs.GBP; 
 }
 
-// Exportar todas las funciones
+
 module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
